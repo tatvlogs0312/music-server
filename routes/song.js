0db6381da5e5523ba1c125b48f5f1a77542f7fbb
tuanta <tanhtuan093@gmail.com>
2023-10-18 10:16:29 +0700

    update

--- routes/song.js
+++ routes/song.js
@@ -1,16 +1,15 @@
-var express = require('express');
-var router = express.Router();
-var config = require('../connections/ConnectDB')
+const express = require("express");
+const router = express.Router();
+const SongService = require("../lib/services/SongService");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    const query = 'select * from song'
-    config.query(query, (err, result) => {
-        if (err) {
-            res.status(500).send(err)
-        }
-        res.status(200).send(result.rows)
-    })
+router.get("/", function (req, res, next) {
+  SongService.GetSong()
+    .then((data) => res.send(data).status(200))
+    .catch((error) => {
+      console.log(error);
+      res.send({ error: error.code, message: "Có lỗi xảy ra!" }).status(500);
+    });
 });
 
 module.exports = router;
