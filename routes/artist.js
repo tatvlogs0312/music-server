73b46356534632120a23dbde67792b7ca6b410dd
tuanta <tanhtuan093@gmail.com>
2023-10-22 18:46:31 +0700

    //api lấy ca sĩ theo id

--- routes/artist.js
+++ routes/artist.js
@@ -3,15 +3,27 @@ const router = express.Router();
 const ArtistService = require("../lib/services/ArtistService");
 
 router.get("/", function (req, res, next) {
-    ArtistService.GetAllArtist()
+  ArtistService.GetAllArtist()
     .then((data) => res.send(data).status(200))
     .catch((error) => res.send(error).status(500));
 });
 
 router.get("/limit", function (req, res, next) {
-    ArtistService.Get10Artist()
+  ArtistService.Get10Artist()
     .then((data) => res.send(data).status(200))
     .catch((error) => res.send(error).status(500));
+});
+
+/**
+ * API Lấy ca sĩ theo id
+ */
+router.get("/:id", async (req, res, next) => {
+  try {
+    const data = await ArtistService.GetArtistDataByID(req.params.id);
+    res.send(data).status(200);
+  } catch (error) {
+    res.send(error).status(500);
+  }
 });
 
 module.exports = router;
